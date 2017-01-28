9dcfea0add20cdf974a7bfe0935346a7efb98d45
Jason Veng <jasonveng@outlook.com>
2017-01-28 14:00:22 +1300

    add newsapi.org attribution link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 export default function Footer() {
   return (
     <div>
       <footer className="campfire_footer">
         <p>&copy; 2017 Campfire Limited, all rights reserved. Campfire logo is trademarks and/or registered trademarks of Campfire Limited</p>
+        <br></br>
+        powered by<Link to="https://newsapi.org/"> NewsAPI.org</Link>
       </footer>
     </div>
   )
