0e9c42c51bce8cc44e014b50441ccb0592f9c333
sengcheav <sengcheav@yahoo.com>
2017-02-02 22:43:38 +1300

    add pg

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const path = require('path')
 const morgan = require('morgan')
 const request = require('request')
 
+var pg = require('pg')
+var connectionString = process.env.DATABASE_URL
+var client = new pg.Client(connectionString)
+
+client.connect()
+
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -18,6 +24,20 @@ app.get('/v1/news', (req, res) => {
     !error && response.statusCode === 200 ? res.send(body) : console.log(error)
   })
 })
+
+var requestLoop = setInterval(function(){
+  var result = [] ;
+  request(url, (error, response, body) => {
+    if(!error && response.statusCode == 200){
+         console.log('sucess!');
+         result = body.articles ;
+
+     }else{
+         console.log('error' + response.statusCode);
+     }
+  })
+
+}, 5000);
 
 app.get('*', (request, response) => {
   response.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
