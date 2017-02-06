37e317e9b9205b3193970880b0523553e9ef72e0
sengcheav <sengcheav@yahoo.com>
2017-02-06 16:15:03 +1300

    now newsfeed do get request from campfire.news/api/v1/newsnow

--- src/components/NewsFeed.jsx
+++ src/components/NewsFeed.jsx
@@ -41,7 +41,7 @@ class NewsFeed extends React.Component {
               publishedAt: json[i].publishedAt
             };
             //*/
-            console.log ( "img url "+ JSON.stringify(each.urlToImage) );
+            console.log ( "img url "+ each.urlToImage.toString() );
             array.push(each);
         }
         console.log ( "this array "+ array.length);
