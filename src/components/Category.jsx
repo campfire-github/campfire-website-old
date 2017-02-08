b48515518a560ccb1d0a43b026f20724e161c6da
sengcheav <sengcheav@yahoo.com>
2017-02-08 23:20:54 +1300

    add category component

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -24,6 +24,7 @@ class Category extends React.Component{
   render() {
 
     var title = this._getTitleAsLink() ;
+    console.log(this.props);
     return(
       <div className ='category col-md-4'>
         <h2>this.props.categoryname</h2>
