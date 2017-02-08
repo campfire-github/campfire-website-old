c68978385e07ff5114511c91f3eabdbef3d8f191
sengcheav <sengcheav@yahoo.com>
2017-02-08 23:11:37 +1300

    add category component

--- src/components/FrontPage2.jsx
+++ src/components/FrontPage2.jsx
@@ -5,6 +5,7 @@ import World from './World.jsx';
 import HeadLine from './HeadLine.jsx';
 import Grid from './Grid.jsx';
 import Title from './Title.jsx'
+import Category from './Category.jsx'
 import jQuery from '../../dist/js/jquery-3.1.1.js';
 
 class FrontPage2 extends React.Component{
