022c3e15be08fb43114c78000700aec4554c5060
sengcheav <sengcheav@yahoo.com>
2017-02-08 23:49:25 +1300

    add category component

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -13,6 +13,7 @@ class Category extends React.Component{
   _getTitleAsLink() {
 
     var a = this.state.all ;
+    console.log("a" + a );
     return a.map((each)=>{
       return (
         <Title title={each.title} url={each.url} key={each.url}></Title>
@@ -28,7 +29,6 @@ class Category extends React.Component{
 
     var title = this._getTitleAsLink() ;
     console.log(this.props);
-    console.log ("title " + title) ;
     return(
       <div className ='category col-md-4'>
         <h2>{this.props.categoryname}</h2>
