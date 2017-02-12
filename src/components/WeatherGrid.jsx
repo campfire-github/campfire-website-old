140b9780f32633cc6804b633208626d77c05376c
sengcheav <sengcheav@yahoo.com>
2017-02-12 17:46:14 +1300

    add weathergrid weather

--- src/components/WeatherGrid.jsx
+++ src/components/WeatherGrid.jsx
@@ -13,7 +13,7 @@ class WeatherGrid extends React.Component{
     var icon = "http://openweathermap.org/img/w/"+this.props.icon +".png";
     return (
       <div className = "weather_grid col-md-2 col-xs-2">
-        <img className = "col-md-10 col-xs-10" src ={icon} ></img>
+        <img className = "temp-p col-md-10 col-xs-10" src ={icon} ></img>
         <h2 className = "temp-p">{this.props.temp}</h2>
         <p className = "temp-p">{this.props.description}</p>
         <p className = "tempdescription-p">{this.props.wind}</p>
