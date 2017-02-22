df1556f4c42fee90ab1c6dca84c907af14d6104b
sengcheav <sengcheav@yahoo.com>
2017-02-22 15:19:37 +1300

    add link

--- src/components/Technology.jsx
+++ src/components/Technology.jsx
@@ -11,17 +11,50 @@ class Technology extends React.Component{
     }
   }
 
+  _fetchNews(){
+    console.log('fetching news')
+    var urllink = "http://www.campfire.news/api/v1/newsnow";
+    jQuery.ajax({
+      method:'GET',
+      url: urllink,
+      beforeSend: function (xhrObj) {
+         xhrObj.setRequestHeader("Content-Type", "application/json");
+      },
+      data:"{body}",
+      success :(result)=>{
+        var json = JSON.parse(result);
+        var all = [] ;
+        for (var i = 0; i<json.length; i++){
+            if( json[i].source === "techcrunch"){
+              all.push(json[i]);
+            }
+        }
+        this.setState({technology:tech});
+      }
+    })
+  }
+
+  _getAllNews() {
+    console.log("get all ");
+    return this.state.tech.map((each)=>{
+      return(
+        <Grid title={each.title} description={each.description} url={each.url} urlToImage={each.urlToImage} key={each.url}></Grid>
+      );
+    });
+  }
 
 
+  componentWillMount() {
+    this._fetchNews() ;
+  }
+  componentDidMount() {}
 
   render() {
-    var alltech = this._getAllTech() ;
+    var alltech = this._getAllNews() ;
     console.log(this.props.whatis);
     return(
       <div>
-
         <HeadLine headline = "Technology Stuff"/>
-        <p>{this.props.whatis}</p>
         <section className="row">
           {alltech}
         </section>
