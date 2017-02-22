97f89936ecf68ba11b6818bf1e3b0b090ae55f21
sengcheav <sengcheav@yahoo.com>
2017-02-22 17:10:47 +1300

    add link

--- src/index.jsx
+++ src/index.jsx
@@ -7,13 +7,16 @@ import App from './components/App.jsx'
 import Technology from './components/Technology.jsx'
 import World from './components/World.jsx'
 import Entertainment from './components/Entertainment.jsx'
+import Sport from './components/Sport.jsx'
 import NotFound from './components/NotFound.jsx'
+
 
 render((
   <Router history={browserHistory}>
     <Route path="/" component={App} />
     <Route path="/entertainment" component={Entertainment} />
     <Route path="/technology" component={Technology} />
+    <Route path="/sport" component={Sport} />
     <Route path="/world" component={World} />
     <Route path="/*" component={NotFound} />
 
