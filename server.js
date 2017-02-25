251ade537243780201f71ddcd4c7bfa72f401d8a
sengcheav <sengcheav@yahoo.com>
2017-02-25 13:44:20 +1300

    newsnow doesnt delete old news

--- server.js
+++ server.js
@@ -190,7 +190,7 @@ var deleteAndInsertNewsnow = function(){
               selectquery.on('err',function(err){ console.log("err in selecting : "+ err)})
               selectquery.on('row', function(res){
                 count = res.count;
-              //  console.log( "result " + res.count ) ;
+                console.log( "result " + res.count ) ;
               })
               if( count ==0){
                 var query = client.query('INSERT INTO newsnow (author,title,url,urlToImage,publishedAt,source,description,insertDate )VALUES ($1,$2,$3,$4,$5,$6,$7,$8)',[json.articles[i].author,json.articles[i].title,json.articles[i].url,json.articles[i].urlToImage,json.articles[i].publishedAt, json.source, json.articles[i].description, today]);
