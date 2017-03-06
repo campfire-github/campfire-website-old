ab6de70a88f47c4bf42c68d95e14c32c824546ff
sengcheav <sengcheav@yahoo.com>
2017-03-07 11:38:00 +1300

    new version of server

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -7,7 +7,7 @@ class Grid extends React.Component{
       <div className="Grid col-md-4 col-sm-12 col-xs-12">
         <img className="col-md-3 col-sm-3 col-xs-3" src={this.props.urlToImage}></img>
         <a className="col-md-8 col-sm-8 col-xs-8" href={this.props.url} target="_blank">
-          <p>{this.props.title}</p>
+          <h4 className = "grid-h4">{this.props.title}</h4>
         </a>
 
 
