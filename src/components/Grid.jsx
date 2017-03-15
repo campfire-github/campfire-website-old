16542ca7dc5bbefa8df02232d1e8916a7b4e05da
sengcheav <sengcheav@yahoo.com>
2017-03-15 17:19:33 +1300

    prvent right click on img

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 
 class Grid extends React.Component{
+
+
+  _handleClick (e){
+     console.log(e.button);
+     if((e.button == 2)){
+      return false ;
+
+     }
+   }
+
+
   render() {
     console.log ( "image url "+ this.props.urlToImage);
     return (
       <div className="Grid col-md-4 col-sm-12 col-xs-12">
-        <img className="col-md-3 col-sm-3 col-xs-3" src={this.props.urlToImage}></img>
+        <img className="col-md-3 col-sm-3 col-xs-3" src={this.props.urlToImage} onClick={this._handleClick.bind(this)} ></img>
         <a className="col-md-9 col-sm-9 col-xs-9" href={this.props.url} target="_blank">
           <h4 className = "grid-h4">{this.props.title}</h4>
         </a>
