7ed0c12a5053273d91639ad206a5e02d878ae40b
sengcheav <sengcheav@yahoo.com>
2017-03-22 20:17:38 +1300

    add masonry layout

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -15,7 +15,7 @@ class Grid extends React.Component{
   render() {
     console.log ( "image url "+ this.props.urlToImage);
     return (
-      <div className="Grid col-md-4 col-sm-12 col-xs-12">
+      <div className="Grid col-md-4 col-sm-6 col-xs-12">
         <img className="col-md-3 col-sm-3 col-xs-3" src={this.props.urlToImage} onClick={this._handleClick.bind(this)} ></img>
         <a className="col-md-9 col-sm-9 col-xs-9" href={this.props.url} target="_blank">
           <h4 className = "grid-h4">{this.props.title}</h4>
