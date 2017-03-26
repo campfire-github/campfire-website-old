1e06d4f5c351d229b729eb86f285752b3d104b13
sengcheav <sengcheav@yahoo.com>
2017-03-26 15:28:27 +1300

    add google ad

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,12 +8,12 @@ const Header = () => (
         <div align="center">
           <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
           <script>
-            (adsbygoogle = window.adsbygoogle || []).push({
+            {(adsbygoogle = window.adsbygoogle || []).push({
               google_ad_client: "ca-pub-8666378670828451",
               enable_page_level_ads: true
-            });
+            });}
           </script>
-        </div > 
+        </div >
     </header>
   </div>
 )
