a6cb83fcaad6d7ea5cd6ab57d46145c89e750239
sengcheav <sengcheav@yahoo.com>
2017-04-03 16:58:09 +1200

    add navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router'
+import Navbar from './NavBar.jsx'
 
 const Header = () => (
   <div>
     <header>
+      <NavBar></NavBar>
       <h1><Link to="/">campfire news</Link></h1>
-        
+
     </header>
   </div>
 )
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Link } from 'react-router';
+class NavBar extends React.Component{
+
+
+  render(){
+    return (
+
+      <nav className="navbar navbar-default navbar-fixed-top ">
+        <div className="container-fluid">
+
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+            <a className="navbar-brand" href="/"> CAMPFIRE NEWS</a>
+          </div>
+
+          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+
+              <ul className="nav navbar-nav  navbar-right">
+
+                  <li><Link to="/world">World</Link></li>
+                  
+                  <li className="dropdown">
+                    <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Contact <span className="caret"></span></a>
+                    <ul className="dropdown-menu">
+                      <li><a href="#">Address</a></li>
+                      <li><a href="#">Phone</a></li>
+                      <li><a href="#">Something else</a></li>
+                      <li role="separator" className="divider"></li>
+                      <li><a href="#">Business hours</a></li>
+                    </ul>
+                  </li>
+
+              </ul>
+          </div>
+
+
+        </div>
+      </nav>
+
+
+    )
+
+  }
+}
+export default NavBar;
