7b4099bb57b7798cb5131a2d3411e13ed7106355
sengcheav <sengcheav@yahoo.com>
2017-04-03 15:18:23 +1200

    add national geographic page

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,8 +1,8 @@
 import React from 'react'  ;
 import Title from './Title.jsx'
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router'
 
-class Category extends React.Component{ // 
+class Category extends React.Component{ //
   constructor(){
     super() ;
     this.state = {
