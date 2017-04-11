74a4f6af1b20d923a57a8901215f1ce923fa8155
sengcheav <sengcheav@yahoo.com>
2017-04-11 15:55:45 +1200

    add search result

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -10,7 +10,7 @@ class Grid extends React.Component{
     return (
       <div className="Grid col-md-4 col-sm-6 col-xs-12">
         <img className="grid-img col-md-3 col-sm-3 col-xs-3" src={this.props.urlToImage}  ></img>
-        <a className="col-md-9 col-sm-9 col-xs-9" href={this.props.url} target="_blank">
+        <a className="grid-a col-md-9 col-sm-9 col-xs-9" href={this.props.url} target="_blank">
           <h4 className = "grid-h4">{this.props.title}</h4>
         </a>
       </div>
