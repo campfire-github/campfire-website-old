e37325af8a1ee8e903dc19d25e9672e012fe9f8a
sengcheav <sengcheav@yahoo.com>
2017-04-11 16:23:07 +1200

    enter to start searching

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -69,7 +69,7 @@ class Form extends React.Component{
 
         <div className = "col-md-12 col-sm-12 col-xs-12">
 
-          <form className="form-horizontal col-md-12 col-sm-12 col-xs-12" onKeyPress={this.checkSubmit.bind(this)}>
+          <form type= "submit" className="form-horizontal col-md-12 col-sm-12 col-xs-12" onKeyPress={this.checkSubmit.bind(this)}>
             <input className = "col-md-9 col-sm-9 col-xs-12" type="text"  onChange={this.updateInputValue.bind(this)} onKeyPress={this.checkSubmit.bind(this)}/>
             <button type="button" onClick={this.onSubmit.bind(this)} className="search-button">Search</button>
           </form>
