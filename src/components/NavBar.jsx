ce790b329309b8e32e3adcd89d1cd3ad9d198649
sengcheav <sengcheav@yahoo.com>
2017-04-11 22:49:22 +1200

    enter to start searching

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -44,7 +44,7 @@ class NavBar extends React.Component{
               <span className="icon-bar"></span>
               <span className="icon-bar"></span>
             </button>
-            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false" onClick = {this.showModalPopup.bind(this)}> &#128269; </button>
+            <span  className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false" onClick = {this.showModalPopup.bind(this)}> &#128269; </span>
             <a className="navbar-brand" href="/"> CAMPFIRE NEWS</a>
           </div>
 
