56f989d9f60af0a7ba61f45def57b24090cbf543
sengcheav <sengcheav@yahoo.com>
2017-04-11 12:59:45 +1200

    add search result

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -3,25 +3,14 @@ import React from 'react'
 class Grid extends React.Component{
 
 
-  _handleClick (e){
-     console.log(e.button);
-     if((e.button == 0)){
-
-
-     }else {return false ;}
-   }
-
-
   render() {
-    console.log ( "image url "+ this.props.urlToImage);
+    //console.log ( "image url "+ this.props.urlToImage);
     return (
       <div className="Grid col-md-4 col-sm-6 col-xs-12">
-        <img className="col-md-3 col-sm-3 col-xs-3" src={this.props.urlToImage} onClick={this._handleClick.bind(this)} ></img>
+        <img className="col-md-3 col-sm-3 col-xs-3" src={this.props.urlToImage}  ></img>
         <a className="col-md-9 col-sm-9 col-xs-9" href={this.props.url} target="_blank">
           <h4 className = "grid-h4">{this.props.title}</h4>
         </a>
-
-
       </div>
     )
   }
